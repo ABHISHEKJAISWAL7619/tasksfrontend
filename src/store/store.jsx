5ee68dbc2546d9bfe8c70007c799b32b807fad7e
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2025-04-24 13:52:58 +0530

    second

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import UserSlice from "..store/UserSlice"
+import userReducer from './userslice';
 
 export const store = configureStore({
   reducer: {
-    // user: userReducer, 
-    user: UserSlice, 
+    user: userReducer, 
   },
 });
 
